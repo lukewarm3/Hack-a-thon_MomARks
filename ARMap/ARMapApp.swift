345f34e1738307de6a840d4815bdc1eb4578f825
lukewarm3 <lcao3@nd.edu>
2025-03-21 18:49:54 -0400

    Initial Commit

---
+++ ARMap/ARMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARMapApp.swift
+//  ARMap
+//
+//  Created by Luke Cao on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ARMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARMap
+//
+//  Created by Luke Cao on 3/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
